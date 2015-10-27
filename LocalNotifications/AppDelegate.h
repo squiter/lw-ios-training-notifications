fc4760331caaab56c3887165f79bec6a7e8ebc32
Brunno dos Santos <squiter85@gmail.com>
2015-10-27 16:13:56 -0200

    Initial Commit

---
+++ LocalNotifications/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalNotifications
+//
+//  Created by Treinamento Mobile on 10/27/15.
+//  Copyright © 2015 Treinamento Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalNotifications/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalNotifications
+//
+//  Created by Treinamento Mobile on 10/27/15.
+//  Copyright © 2015 Treinamento Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
